3025d14552f4523b9e08e5a7c9bcaed03f15c9a0
Aviral Sharma <aviral16072000@gmail.com>
2024-07-04 00:58:12 +0530

    Changes after deployment

--- src/SubComponents/About.jsx
+++ src/SubComponents/About.jsx
@@ -7,10 +7,11 @@ const About = () => {
         <div className="first_container">
           <div className="content">
           <h1>ABOUT US</h1>
-          <p>
-          Jaycee Punching Solutions Pvt. Ltd. is an offshoot of the renowned Jaycee Group, established in the early 1950s. Beginning with the business of trading of bearings, the Group today is a sprawling business with an annual turnover of over 2500 crores. Thanks to the firm commitment to deliver quality, with a customer-centric approach, the Group has expanded to several verticals and interests and is a well-known name in specialized manufacturing.
-Jaycee Punching Solutions, established as a separate entity in 2009, is taking the legacy of excellence forward and is poised to be the leading player in energy solutions and electrical stamping in India.
-And for good reason- with multiple state-of-the-art manufacturing facilities, with world-class assembly and testing processes, only the best is delivered. As an authorized OEM for Eicher Engines (TMTL), Jaycee has manufactured and sold more than 75,000 generators in less than a decade.
+          <span>Jaycee Punching Solutions Pvt. Ltd. is an offshoot of the renowned Jaycee Group, established in the early 1950s.</span>
+          <p>Beginning with the business of trading of bearings, the Group today is a sprawling business with an annual turnover of over 2500 crores.</p> 
+          <p>Thanks to the firm commitment to deliver quality, with a customer-centric approach, the Group has expanded to several verticals and interests and is a well-known name in specialized manufacturing.</p>
+          <p>Jaycee Punching Solutions, established as a separate entity in 2009, is taking the legacy of excellence forward and is poised to be the leading player in energy solutions and electrical stamping in India.</p>
+          <p>And for good reason- with multiple state-of-the-art manufacturing facilities, with world-class assembly and testing processes, only the best is delivered. As an authorized OEM for Eicher Engines (TMTL), Jaycee has manufactured and sold more than 75,000 generators in less than a decade.
           </p>
           </div>
           <button>We strive to offer you best services for all your needs!</button>
